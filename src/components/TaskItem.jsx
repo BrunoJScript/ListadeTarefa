29fa0f09ac3c6ff772b58fee9afedf13a115fbd1
BrunoJScript <shootingworldglockpt@gmail.com>
2025-06-24 10:52:34 -0300

    checkbox update

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
+
 const TaskItem = ({ task, removeFromList }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked((prev) => !prev);
+  };
+
   return (
-    <div className="taskItem">
-      <p>{task}</p>
-      <button onClick={() => removeFromList(task)}>Remover</button>
+    <div className="taskItem" style={{ marginBottom: "10px" }}>
+      <input
+        className="checkbox"
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+      />
+      <span
+        style={{
+          marginLeft: "10px",
+          textDecoration: isChecked ? "line-through" : "none",
+          color: isChecked ? "gray" : "black",
+        }}
+      >
+        {task}
+      </span>
+      <button
+        onClick={() => removeFromList(task)}
+        style={{ marginLeft: "10px" }}
+      >
+        Remover
+      </button>
     </div>
   );
 };
 
 export default TaskItem;
+
+// const TaskItem = ({ task, removeFromList }) => {
+//   return (
+//     <div className="taskItem">
+//       <p>{task}</p>
+//       <button onClick={() => removeFromList(task)}>Remover</button>
+//     </div>
+//   );
+// };
